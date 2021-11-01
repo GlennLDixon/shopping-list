df9829d757071d59abdbbe5b6b61efa28af62271
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-10-31 21:48:42 -0500

    first commit

---
+++ shopping.js
@@ -0,0 +1,22 @@
+const shoppingList = [
+    {
+        id: 1,
+        food: "Apple",
+        allergic: false,
+        price: 1.30
+    },
+    {
+        id: 2,
+        food: "Pair",
+        allergic: false,
+        price: 1.15
+    },
+    {
+        id: 3,
+        food: "Pineapple",
+        allergic: false,
+        price: 2.10
+    }
+]
+
+console.log(shoppingList)
